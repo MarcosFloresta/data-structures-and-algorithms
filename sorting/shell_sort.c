18730a3c22b843979349ff563ae2e77a95f8712c
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-20 19:39:30 -0300

    Create shell_sort.c

---
+++ sorting/shell_sort.c
@@ -0,0 +1,42 @@
+/*
+shellSort(array, size)
+  for interval i <- size/2n down to 1
+    for each interval "i" in array
+        sort all the elements at interval "i"
+end shellSort
+*/
+
+// Shell Sort in C programming
+#include <stdio.h>
+void shellSort(int array[], int n)
+{
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i += 1)
+        {
+            int temp = array[i];
+            int j;
+            for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+            {
+                array[j] = array[j - gap];
+            }
+            array[j] = temp;
+        }
+    }
+}
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d  ", array[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int data[] = {9, 8, 3, 7, 5, 6, 4, 1};
+    int size = sizeof(data) / sizeof(data[0]);
+    shellSort(data, size);
+    printf("Sorted array: \n");
+    printArray(data, size);
+}
