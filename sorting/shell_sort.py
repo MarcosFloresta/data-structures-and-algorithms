f898ee8eaaa6ed80faf069a4073b55bedc852067
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-20 19:39:24 -0300

    Create shell_sort.py

---
+++ sorting/shell_sort.py
@@ -0,0 +1,19 @@
+# Python3 program for implementation of Shell Sort
+def shellSort(array, n):
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = array[i]
+            j = i
+            while j >= gap and array[j - gap] > temp:
+                array[j] = array[j - gap]
+                j -= gap
+            array[j] = temp
+        gap //= 2
+
+
+data = [9, 8, 3, 7, 5, 6, 4, 1]
+size = len(data)
+shellSort(data, size)
+print('Sorted Array in Ascending Order:')
+print(data
