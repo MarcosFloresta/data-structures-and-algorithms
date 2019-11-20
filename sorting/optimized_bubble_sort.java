b6db56cd19a9ea9a34f3a599f4f197acc69717c1
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-20 19:39:59 -0300

    Create optimized_bubble_sort.java

---
+++ sorting/optimized_bubble_sort.java
@@ -0,0 +1,31 @@
+
+// Optimized bubble sort in Java
+import java.util.Arrays;
+
+class BubbleSort {
+    void bubbleSort(int array[]) {
+        int size = array.length;
+        for (int i = 0; i < size - 1; i++) {
+            boolean swapped = true;
+            for (int j = 0; j < size - i - 1; j++) {
+                // To sort in descending order, change > to < in this line.
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = false;
+                }
+            }
+            if (swapped == true)
+                break;
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] data = { -2, 45, 0, 11, -9 };
+        BubbleSort bs = new BubbleSort();
+        bs.bubbleSort(data);
+        System.out.println("Sorted Array in Ascending Order:");
+        System.out.println(Arrays.toString(data));
+    }
+}
