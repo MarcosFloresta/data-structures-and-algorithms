fa02e901d7077661c0b9e6d03885c9736c64bf8a
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-20 19:39:27 -0300

    Create shell_sort.java

---
+++ sorting/shell_sort.java
@@ -0,0 +1,27 @@
+
+// Shell sort in Java programming
+import java.util.Arrays;
+
+class ShellSort {
+    void shellSort(int array[], int n) {
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int temp = array[i];
+                int j;
+                for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                    array[j] = array[j - gap];
+                }
+                array[j] = temp;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] data = { 9, 8, 3, 7, 5, 6, 4, 1 };
+        int size = data.length;
+        ShellSort ss = new ShellSort();
+        ss.shellSort(data, size);
+        System.out.println("Sorted Array in Ascending Order: ");
+        System.out.println(Arrays.toString(data));
+    }
+}
