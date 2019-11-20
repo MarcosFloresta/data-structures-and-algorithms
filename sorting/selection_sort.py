0887c014aae3ae854c9076b2ca1e415c26418764
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-20 19:39:32 -0300

    Create selection_sort.py

---
+++ sorting/selection_sort.py
@@ -0,0 +1,16 @@
+# Selection sort in Python
+def selectionSort(array, size):
+    for step in range(size):
+        min_idx = step
+        for i in range(step + 1, size):
+            # To sort in descending order, change > to < in this line.
+            if array[i] < array[min_idx]:
+                min_idx = i
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
+
+
+data = [-2, 45, 0, 11, -9]
+size = len(data)
+selectionSort(data, size)
+print('Sorted Array in Ascending Order:\n')
+print(data)
