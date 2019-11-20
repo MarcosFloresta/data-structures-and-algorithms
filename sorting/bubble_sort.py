19ae31fa8e0964750d48d718c7d8dcf166b92370
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-20 19:40:46 -0300

    Create bubble_sort.py

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,13 @@
+# Bubble sort in Python
+def bubbleSort(array):
+    for i in range(len(array)):
+        for j in range(0, len(array) - i - 1):
+            # To sort in descending order, change > to < in this line.
+            if array[j] > array[j + 1]:
+                (array[j], array[j + 1]) = (array[j + 1], array[j])
+
+
+data = [-2, 45, 0, 11, -9]
+bubbleSort(data)
+print('Sorted Array in Ascending Order:')
+print(data)
