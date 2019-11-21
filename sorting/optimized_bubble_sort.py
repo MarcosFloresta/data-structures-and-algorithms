78c544187107fc117ad4d5724bc3f5785a46d751
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-21 20:09:58 -0300

    Update optimized_bubble_sort.py

--- sorting/optimized_bubble_sort.py
+++ sorting/optimized_bubble_sort.py
@@ -1,5 +1,5 @@
 # Python Program To Sort data in ascending order using bubble sort.
-def bubbleSort(array):
+def bubble_sort(array):
     for i in range(len(array)):
         swapped = True
         for j in range(0, len(array) - i - 1):
@@ -12,6 +12,6 @@ def bubbleSort(array):
 
 
 data = [-2, 45, 0, 11, -9]
-bubbleSort(data)
+bubble_sort(data)
 print('Sorted Array in Ascending Order:')
 print(data)
